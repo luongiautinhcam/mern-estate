eee772086815db4f9b2868a739bf9aec3f3689b6
Vinh <tranthanhvinh.info@gmail.com>
2024-01-20 04:48:49 +0700

    complete delete listing function

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,9 +1,28 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
     return res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Lisitng not found"));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only delte your own listings"));
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    return res.status(200).json("Listing delete success");
   } catch (error) {
     next(error);
   }
