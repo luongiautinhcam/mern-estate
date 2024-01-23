ae23bcb063fa92bd47d98cad6a4d386f59e6209a
Vinh <tranthanhvinh.info@gmail.com>
2024-01-23 15:24:16 +0700

    create api update listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -27,3 +27,25 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Lisitng not found"));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings"));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json("Listing update success");
+  } catch (error) {
+    next(error);
+  }
+};
